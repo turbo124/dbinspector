b356c4afa432b6579bf4501bcf000c1ceb818fac
= <turbo124@gmail.com>
2021-08-28 20:01:15 +1000

    Add Tests

---
+++ tests/DBTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DBInspector\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+
+class DBTest extends TestCase
+{
+    /** @test */
+	public function testValidInstanceType()
+	{
+		$this->assertTrue(true);
+	}
+}
