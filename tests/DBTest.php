8a77f796c15754f0c304b642330c7ef33b041192
= <turbo124@gmail.com>
2021-08-28 20:24:13 +1000

    Working on db connections

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -10,6 +10,28 @@ class DBTest extends TestCase
     /** @test */
 	public function testValidInstanceType()
 	{
+		
+		$connectionParams = array(
+			'dbname' => 'db-ninja-01',
+			'user' => 'ninja',
+			'password' => 'ninja',
+			'host' => 'localhost',
+			'driver' => 'pdo_mysql',
+		);
+
+		$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+
+		$platform = $conn->getDatabasePlatform();
+
+		$platform->registerDoctrineTypeMapping('enum', 'string');
+
+		$sm = $conn->getSchemaManager();
+		// $databases = $sm->listDatabases();
+
+		$tables = $sm->listTables();
+
+		print_r($tables);
+
 		$this->assertTrue(true);
 	}
 }
